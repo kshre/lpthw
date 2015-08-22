0af81e40894f50cacc34397906f24fb7e23f53da
Saurabh Kulshreshtha <saurkulsh@gmail.com>
2015-08-23 00:35:19 +0530

    five: other format chars

---
+++ ex5_other_format_chars.py
@@ -0,0 +1,8 @@
+i = 10.0 ** -5
+print "ten to the minus five in exponential format: %e" % i
+print "ten to the minus five in decimal formal: %f" % i
+print "ten to the minus five in string representation: %r" % i
+
+c = 117
+print "character: %c" % c
+
