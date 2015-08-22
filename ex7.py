8a7b7c999e1ef687ca0368d3e5f772e38a951c03
Saurabh Kulshreshtha <saurkulsh@gmail.com>
2015-08-23 03:02:15 +0530

    se7en: drill1 and @last3lines @seeOutput

--- ex7.py
+++ ex7.py
@@ -1,9 +1,14 @@
+# print string
 print "Mary had a little lamb."
+# print format string and embedded string supplied
 print "Its fleece was white as %s." % "snow"
-
+# print string
 print "And everywhere that mary went."
+# print ten dots
 print "." * 10 # what'd that do?
 
+# variables initialized to strings containing single characters
+#CheeseBurger
 end1 = "C"
 end2 = "h"
 end3 = "e"
@@ -17,7 +22,11 @@ end10 = "g"
 end11 = "e"
 end12 = "r"
 
-# watch the comma at the end. try removing it to see what happens
+# prints concatenation of end1 to end6
 print end1 + end2 + end3 + end4 + end5 + end6
+# prints concatenation of end7 to end12
 print end7 + end8 + end9 + end10 + end11 + end12
 
+# comma @ the end suppresses newline
+print end1 + end2 + end3 + end4 + end5 + end6, 
+print end7 + end8 + end9 + end10 + end11 + end12
