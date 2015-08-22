1648a5d9b154be5aa94dfc70c9085a2f81a75492
Saurabh Kulshreshtha <saurkulsh@gmail.com>
2015-08-23 00:01:42 +0530

    five: vim %s to remove commas

--- ex5.py
+++ ex5.py
@@ -6,12 +6,12 @@ my_eyes = 'black'
 my_teeth = 'off-white'
 my_hair = 'black'
 
-print "Let's talk about %s.", % my_name
-print "He's %d years old.", % my_age
-print "He's %d inches tall.", % my_height
-print "He's %d kilos heavy.", % my_weight
+print "Let's talk about %s." % my_name
+print "He's %d years old." % my_age
+print "He's %d inches tall." % my_height
+print "He's %d kilos heavy." % my_weight
 print "Actually it is quite heavy."
-print "He's got %s eyes and %s hair.", % (my_eyes, my_hair)
+print "He's got %s eyes and %s hair." % (my_eyes, my_hair)
 
 # this line is tricky, try getting it exactly right
-print "If I add %d, %d and %d then I get %d", % (my_age, my_height, my_weight, my_age + my_height + my_weight) 
+print "If I add %d, %d and %d then I get %d" % (my_age, my_height, my_weight, my_age + my_height + my_weight) 
