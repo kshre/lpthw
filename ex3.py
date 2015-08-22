931b47403f62c82c486f4e0680bd6d9d9d100b7e
Saurabh Kulshreshtha <saurkulsh@gmail.com>
2015-08-22 12:44:23 +0530

    study_drillz_ex3_part1_commentingItAll

--- ex3.py
+++ ex3.py
@@ -1,23 +1,34 @@
+# Print the message in quotes
 print "Now I will count my chickens:"
 
+# Print "Hens" followed by evaluated result of the math expression
 print "Hens", 25 + 30 / 6
+# Print "Roosters" followed by evaluated result of the math expression
 print "Roosters", 100 - 25 * 3 % 4
 
+# Print the string to terminal
 print "Now I will count the eggs:"
 
+# Print to terminal the result of the evaluation of the expression
 print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
 
+# Print the string to terminal
 print "Is it true that 3 + 2 < 5 - 7?"
 
+# Print the result of the expression(boolean)
 print 3 + 2 < 5 - 7
 
+# Print the string followed by a whitespace character followed by the result of the math expressions
 print "What is 3 + 2?", 3 + 2
 print "What is 5 - 7?", 5 - 7
 
+# Print to terminal the string
 print "Oh, that's why it's False."
 
+# Print to terminal the string of characters enclosed in the quotes.
 print "How about some more."
 
+# Print the strings followed by the result of the boolean expressions
 print "Is it greater?", 5 > -2
 print "Is it greater or equal?", 5 >= -2
 print "Is it less than or equal?", 5 <= -2
