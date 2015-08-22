7d83f4644fd250ce143a2970675280933e98a43d
Saurabh Kulshreshtha <saurkulsh@gmail.com>
2015-08-23 00:16:53 +0530

    ex5: conversions.drillz2

--- ex5.py
+++ ex5.py
@@ -6,6 +6,15 @@ eyes = 'black'
 teeth = 'off-white'
 hair = 'black'
 
+# conversions
+one_inch_to_cm = 2.54
+one_kilo_to_pound = 2.20
+weight_pounds = weight * one_kilo_to_pound
+height_cms = height * one_inch_to_cm
+print "Weight:", weight_pounds, "lbs, Height:", height_cms, "centimeters."
+print
+
+# print to terminal
 print "Let's talk about %s." % name
 print "He's %d years old." % age
 print "He's %d inches tall." % height
