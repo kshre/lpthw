487756e353ed0a00acc9264217dce2ce8f3f2716
Saurabh Kulshreshtha <saurkulsh@gmail.com>
2015-08-23 00:07:32 +0530

    ex5: renamed variables

--- ex5.py
+++ ex5.py
@@ -1,17 +1,17 @@
-my_name = 'Saurabh Kulshreshtha'
-my_age = 23 # not a lie
-my_height = 71 # inches
-my_weight = 72 # kgs
-my_eyes = 'black'
-my_teeth = 'off-white'
-my_hair = 'black'
+name = 'Saurabh Kulshreshtha'
+age = 23 # not a lie
+height = 71 # inches
+weight = 72 # kgs
+eyes = 'black'
+teeth = 'off-white'
+hair = 'black'
 
-print "Let's talk about %s." % my_name
-print "He's %d years old." % my_age
-print "He's %d inches tall." % my_height
-print "He's %d kilos heavy." % my_weight
+print "Let's talk about %s." % name
+print "He's %d years old." % age
+print "He's %d inches tall." % height
+print "He's %d kilos heavy." % weight
 print "Actually it is quite heavy."
-print "He's got %s eyes and %s hair." % (my_eyes, my_hair)
+print "He's got %s eyes and %s hair." % (eyes, hair)
 
 # this line is tricky, try getting it exactly right
-print "If I add %d, %d and %d then I get %d" % (my_age, my_height, my_weight, my_age + my_height + my_weight) 
+print "If I add %d, %d and %d then I get %d" % (age, height, weight, age + height + weight) 
