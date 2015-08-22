21c505492f1eaaea8b1d48a703d105f7cbc405fb
Saurabh Kulshreshtha <saurkulsh@gmail.com>
2015-08-23 00:01:17 +0530

    five: vim %s to remove commas

--- ex4.py
+++ ex4.py
@@ -1,17 +1,20 @@
-# initialize cars variable to the value 100
+# cars available
 cars = 100
-# similarly initializing all the following variables to the values specified
-# NOTE: no var keyword required
+# seating capacity of the cars
 space_in_a_car = 4
+# drivers available
 drivers = 30
+# passengers available
 passengers = 90
+# cars that won't be driven due to lack of drivers
 cars_not_driven = cars - drivers
+# cars driven
 cars_driven = drivers
+# max capacity of driven cars
 carpool_capacity = cars_driven * space_in_a_car
+# average number of people to be seated per car today 
 average_passengers_per_car = passengers / cars_driven
 
-# print to terminal string , variable and string again separated by spaces
-# NOTE: the strings are separated by commas in code and spaces in the output
 print "There are", cars, "cars available."
 print "There are only", drivers, "drivers available."
 print "There will be", cars_not_driven, "empty cars today."
