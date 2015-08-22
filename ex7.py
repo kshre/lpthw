4dabc7a2710c1bb14d6c364c308b5c97ab2cad6e
Saurabh Kulshreshtha <saurkulsh@gmail.com>
2015-08-23 02:51:50 +0530

    se7en: comma at the end suppressed newline #print; diff'n'execute previous and this commit

--- ex7.py
+++ ex7.py
@@ -18,6 +18,6 @@ end11 = "e"
 end12 = "r"
 
 # watch the comma at the end. try removing it to see what happens
-print end1 + end2 + end3 + end4 + end5 + end6,
+print end1 + end2 + end3 + end4 + end5 + end6
 print end7 + end8 + end9 + end10 + end11 + end12
 
