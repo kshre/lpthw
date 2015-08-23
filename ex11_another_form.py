e8c92582751fb0afa01aa8e6a5922bcc96c54fdf
Saurabh Kulshreshtha <saurkulsh@gmail.com>
2015-08-23 09:20:28 +0530

    eleven: another form added drill3

---
+++ ex11_another_form.py
@@ -0,0 +1,10 @@
+# This is another form
+print "Hello,"
+print "Could you answer the following questions"
+age = raw_input("How many years old are you? ")
+content = raw_input("Are you content at present? ")
+want = raw_input("What do you want? ")
+
+print "Okay thanks for answering the questions!"
+print "You responses were that you are %r years old" % age
+print "and that you are %r content at present and want %r" % (content, want)
